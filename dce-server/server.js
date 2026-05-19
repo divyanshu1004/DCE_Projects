7dc27e1e09f25839fb8702aacf29cebba4cdc3be
flamingo_o <vedi05962@gmail.com>
2026-05-19 12:10:22 +0530

    fix: hardcode Vercel URL in CORS allowedOrigins

--- dce-server/server.js
+++ dce-server/server.js
@@ -14,6 +14,7 @@ const app = express()
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://dce-projects.vercel.app',
   process.env.CLIENT_URL,
 ].filter(Boolean)
 
